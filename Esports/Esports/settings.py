537e50a35598447cfc85ca23be682e4f75fff76f
Liza <liza.adamyan93@gmail.com>
2019-08-15 14:25:40 +0400

    created a new app named users for a user to login/register

--- Esports/Esports/settings.py
+++ Esports/Esports/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'tournament.apps.TournamentConfig',
+    'users.app.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
