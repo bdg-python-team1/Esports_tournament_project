c078b763ac0888ca1dc87da6eb26b84ceacf3c0b
GevorgArtenyan <53793770+GevorgArtenyan@users.noreply.github.com>
2019-08-15 15:41:39 +0400

    static added

--- Esports/Esports/settings.py
+++ Esports/Esports/settings.py
@@ -34,7 +34,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'tournament.apps.TournamentConfig',
-    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
