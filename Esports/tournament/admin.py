a98f337c23ee25ee41ff71ce0f679074fba34d05
GevorgArtenyan <53793770+GevorgArtenyan@users.noreply.github.com>
2019-08-24 14:32:32 +0400

    site redesigned

--- Esports/tournament/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Contest
-
-# Register your models here.
-class ContestAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug':('name',)}
-
-admin.site.register(Contest, ContestAdmin)
